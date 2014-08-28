359ef69da82ee4a462b3713329c04c7f562d9d1f
jlwrigh1 <jlwrigh1@general2.asu.edu>
2014-08-27 21:38:35 -0700

    Added randomized test case generation.

--- test.cpp
+++ test.cpp
@@ -5,21 +5,37 @@
 #include <random>
 #include <limits>
 #include <cassert>
-TEST(SortTest, Randomized)
+
+class RandomizedTest : public testing::TestWithParam<std::vector<int>> {};
+TEST_P(RandomizedTest, Randomized)
 {
+    auto vs = GetParam();
+    selection_sort(std::begin(vs), std::end(vs));
+    ASSERT_TRUE(std::is_sorted(std::begin(vs), std::end(vs)));
+}
+
+
+std::vector<std::vector<int>> GenerateTestCases()
+{
+    std::vector<std::vector<int>> test_cases;
     std::numeric_limits<int> limits;
 
     std::mt19937 engine(time(0)); // Fixed seed of 0
     std::uniform_int_distribution<int> range_dist(0,150);
     std::uniform_int_distribution<int> element_dist(0, limits.max());
-   
+
     size_t n = 10000;
     auto f = [&](){return element_dist(engine);};
     for(size_t i = 0; i < n; ++i)
     {
         std::vector<int> vs;
         std::generate_n(std::back_inserter(vs), range_dist(engine), f);
-        selection_sort(std::begin(vs), std::end(vs));
-        EXPECT_TRUE(std::is_sorted(std::begin(vs), std::end(vs)));
+        test_cases.push_back(vs);
     }
+    return test_cases;
 }
+
+INSTANTIATE_TEST_CASE_P(
+        GeneralAndSpecial,
+        RandomizedTest,
+        testing::ValuesIn(GenerateTestCases()));
