ed70633887ea4fdcb85c387b804911dc6a8a4c10
Jeremy Wright <jeremy@codestrokes.com>
2014-10-25 19:12:51 -0700

    Started Assignment #4

---
+++ a4/run_callgrind.sh
@@ -0,0 +1,8 @@
+jwright@PhaseShift:~/art/cse565/a1/build$ valgrind --tool=callgrind ./test 
+==4362== Callgrind, a call-graph generating cache profiler
+==4362== Copyright (C) 2002-2013, and GNU GPL'd, by Josef Weidendorfer et al.
+==4362== Using Valgrind-3.10.0.SVN and LibVEX; rerun with -h for copyright info
+==4362== Command: ./test
+==4362== 
+==4362== For interactive control, run 'callgrind_control -h'.
+Running main() from gtest_main.cc
